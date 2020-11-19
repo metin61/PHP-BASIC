697deccda8729b8f8adb7312ac112ef0154f5ac5
metin celik <metin.celik@student.rocva.nl>
2020-11-19 11:10:21 +0100

    if elseif else

--- 5-Conditional_Statements/taak04/index.php
+++ 5-Conditional_Statements/taak04/index.php
@@ -10,13 +10,13 @@
 
         if($prod_besteld < 10) {
             $prijs = 1.50;
-            echo "prijs €1.50". "<br>";
+            echo $prijs;
         } elseif($prod_besteld < 20) {
             $prijs = 1.25;
-            echo"prijs €1.25" . "<br>";
+            echo $prijs;
         } else {
             $prijs = 1.00;
-            echo "prijs €1.00" . "<br>";
+            echo $prijs;
         }
 
        
