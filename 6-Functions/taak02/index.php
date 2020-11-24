563c19cac433d061ed54ceb7e558afb7acaf4ccc
metin celik <metin.celik@student.rocva.nl>
2020-11-24 12:28:11 +0100

    funtion

---
+++ 6-Functions/taak02/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+function telAlleWoorden($lengteZin) {
+        $auto= "Mijn blauwe auto is een mooie snelle wagen";
+        echo strlen($auto);
+    }
+    
+    telAlleWoorden($lengteZin);
+
+    ?>
+</body>
+</html>
